58d3e9291b5c1c7016e713c33c233ab1989afbfe
William Cohen <wcohen@macbook-air-173.mynetworksettings.com>
2025-09-02 17:58:23 -0400

    simplified bigram scores

--- unix-poetry/best-bigrams.sh
+++ unix-poetry/best-bigrams.sh
@@ -1,16 +1,15 @@
 # word frequency
-tr -sc '[:alpha:]' '\n' < data/bluecorpus.txt | sort | uniq -c > /tmp/bluehist.txt
+tr -sc '[:alpha:]' '\n' < data/bluecorpus.txt | sort | uniq -c > /tmp/bluewc.txt
 
 # all bigrams
 tail -n +2  /tmp/bluewords.txt  > /tmp/bluenextwords.txt
-paste /tmp/bluewords.txt /tmp/bluenextwords.txt | sort | uniq -c > /tmp/bluebigrams.txt
+paste /tmp/bluewords.txt /tmp/bluenextwords.txt | sort | uniq -c > /tmp/bluebigram_count.txt
 
-# score them by log (N & freq[xy] / freq[x]*freq[y])
-N="`wc -l < /tmp/bluewords.txt`"
+# score with log(freq[xy] / freq[x]*freq[y])
 
-cat /tmp/bluehist.txt /tmp/bluebigrams.txt |  awk \
+cat /tmp/bluewc.txt /tmp/bluebigram_count.txt |  awk \
     'NF == 2 { freq[$2] = $1}
-     NF == 3 { print log( N * $1  / (freq[$2] * freq[$3]) ),  $2, $3, $1, freq[$2], freq[$3]}' N=$$N > /tmp/scored-phrases.txt
+     NF == 3 { print log( $1  / (freq[$2] * freq[$3]) ),  $2, $3, $1, freq[$2], freq[$3]}' > /tmp/scored-phrases.txt
 
 # filter out infrequent phrases
 cat /tmp/scored-phrases.txt | awk '$4 > 5 {print}' | sort -nr
